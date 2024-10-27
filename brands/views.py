3d50d4a8f2bcc9ce532e1c5906c530956815819b
Wilson Ritt <wilson.ritt@l1consultoria.com.br>
2024-10-27 16:02:39 -0300

    added base templates and started brands view

--- brands/views.py
+++ brands/views.py
@@ -1,3 +1,8 @@
+from django.views.generic import ListView
 from django.shortcuts import render
+from . import models
 
-# Create your views here.
+class BrandListView(ListView):
+    model = models.Brand
+    template_name = 'brand_list.html'
+    context_object_name = 'brands'
