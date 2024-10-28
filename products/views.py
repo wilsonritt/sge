4fe416d97aa719bd0071d1cc98a481e77566d8dd
Wilson Ritt <wilson.ritt@l1consultoria.com.br>
2024-10-27 21:15:30 -0300

    added products interfaces

--- products/views.py
+++ products/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from . import models, forms
+from categories.models import Category
+from brands.models import Brand
 
-# Create your views here.
+class ProductListView(ListView):
+    model = models.Product
+    template_name = 'product_list.html'
+    context_object_name = 'products'
+    paginate_by = 5
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        title = self.request.GET.get('title')
+        serie_number = self.request.GET.get('serie_number')
+        category = self.request.GET.get('category')
+        brand = self.request.GET.get('brand')
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+
+        if serie_number:
+            queryset = queryset.filter(serie_number__icontains=serie_number)
+
+        if category:
+            queryset = queryset.filter(category_id=category)
+
+        if brand:
+            queryset = queryset.filter(brand_id=brand)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['brands'] = Brand.objects.all()
+
+        return context
+
+
+class ProductCreateView(CreateView):
+    model = models.Product
+    template_name = 'product_create.html'
+    form_class = forms.ProductForm
+    success_url = reverse_lazy('product_list')
+
+
+class ProductDetailView(DetailView):
+    model = models.Product
+    template_name = 'product_detail.html'
+
+
+class ProductUpdateView(UpdateView):
+    model = models.Product
+    template_name = 'product_update.html'
+    form_class = forms.ProductForm
+    success_url = reverse_lazy('product_list')
+
+
+class ProductDeleteView(DeleteView):
+    model = models.Product
+    template_name = 'product_delete.html'
+    success_url = reverse_lazy('product_list')
