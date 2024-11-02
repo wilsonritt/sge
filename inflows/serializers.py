5f3d2b977fab912c7e13e3ffa6387988cf3fc5ea
Wilson Ritt <wilson.ritt@l1consultoria.com.br>
2024-11-01 22:11:19 -0300

    created rest api

---
+++ inflows/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from inflows.models import Inflow
+
+class InflowSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Inflow
+        fields = '__all__'
